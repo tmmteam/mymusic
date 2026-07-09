6fab130eeefb9bf52126a24d8122bd89373421be
tmmteam <teamtmm2@gmail.com>
2026-07-09 15:26:38 +0530

    Delete PritiMusic directory

--- PritiMusic/cplugin/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-import time
-from .active import *
-from .queue import *
-from .inline import *
-from .dossier import *
-from .watcher import *
-from .cthumbnail import *
-
-StartTime = time.time()
--- PritiMusic/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .channelplay import *
-from .database import *
-from .decorators import *
-from .extraction import *
-from .formatters import *
-from .inline import *
-from .sys import *
